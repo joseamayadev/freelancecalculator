ed87f99a86abed98b09c245e01d44d7f8b5b7bee
José Antonio Fernández <68615071+syntaxter@users.noreply.github.com>
2021-05-04 19:08:09 -0500

    Calculos de estados en los campos de numeros

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-var mes = document.getElementsByName('mes')[0],
-    validaciones = document.querySelectorAll('input'),
+var validaciones = document.querySelectorAll('input'),
     button = document.getElementById('procesar'),
     resultados = document.querySelector('.resultados'),
     formulario = document.querySelector('form');
 
+
+//seleccion de cada uno de las entradas de texto
+var mes = document.getElementsByName('mes')[0],
+    renta = document.getElementsByName('renta')[0],
+    celular = document.getElementsByName('celular')[0],
+    internet = document.getElementsByName('internet')[0],
+    serviciosFijos = document.getElementsByName('serviciosFijos')[0],
+    vidaOtros = document.getElementsByName('vidaOtros')[0],
+    herramienta = document.getElementsByName('herramienta')[0],
+    dominio = document.getElementsByName('dominio')[0],
+    hosting = document.getElementsByName('hosting')[0],
+    otrosTrabajo = document.getElementsByName('otrosTrabajo')[0],
+    diasPorSemana = document.getElementsByName('diasPorSemana')[0],
+    horasPorDia = document.getElementsByName('horasPorDia')[0],
+    diasVacaciones = document.getElementsByName('horasPorDia')[0];
 
 
 
@@ -17,7 +31,7 @@ function validacionesFormularios() {
             element.classList.remove('require');
             setTimeout(() => {
                 button.classList.remove('procesando');
-                resultados.classList.add('mostrar')
+                resultados.classList.add('mostrar');
             }, 1500);
 
         }
@@ -30,6 +44,64 @@ button.addEventListener('click', (e) => {
     validacionesFormularios();
     setTimeout(() => {
         button.classList.remove('procesando');
+        var gastos = gastosFijos(),
+            trabajo = gastosFijosTrabajo(),
+            totalMes = parseInt(mes.value) + gastos + trabajo,
+            diasXsemana = parseInt(diasPorSemana.value),
+            horasXdia = parseInt(horasPorDia.value),
+            vacaciones = parseInt(diasVacaciones.value);
+
+        var totalAnual = totalMes * 12,
+            totalDiasSemana = diasXsemana * horasXdia,
+            totalhorasMes = totalDiasSemana * 4,
+            totalHorasYear = totalhorasMes * 12,
+            valorPrecioXhora = totalAnual / totalHorasYear;
+
+
+        mostrarResultados(totalMes, totalAnual, totalDiasSemana, totalhorasMes, totalHorasYear, valorPrecioXhora);
+
     }, 1500);
 
-})
+});
+
+function mostrarResultados(totalMes, totalAnual, totalDiasSemana, totalhorasMes, totalHorasYear, valorPrecioXhora) {
+    document.querySelector('.horasYear').innerText = totalMes;
+    document.querySelector('.totalAnual').innerText = totalAnual;
+    document.querySelector('.totalDiasSemana').innerText = totalDiasSemana;
+    document.querySelector('.totalHorasMes').innerText = totalhorasMes;
+    document.querySelector('.totalHorasYear').innerText = totalHorasYear;
+    document.querySelector('.valorPrecioXhora').innerText = valorPrecioXhora.toFixed(2);
+}
+
+//extraer los valores 
+function gastosFijos() {
+    //calculos de gastos fijos
+    var valorRenta = parseInt(renta.value);
+    var valorCelular = parseInt(celular.value);
+    var valorInternet = parseInt(internet.value);
+    var valorServiciosFijos = parseInt(serviciosFijos.value);
+    var valorvidaOtros = parseInt(vidaOtros.value);
+    // console.log(valorMes);
+    // console.log(valorRenta);
+    // console.log(valorCelular);
+    // console.log(valorInternet);
+    // console.log(valorServiciosFijos);
+    // console.log(valorvidaOtros);
+    var totalgastosFijos = valorRenta + valorCelular + valorInternet + valorServiciosFijos + valorvidaOtros;
+    return totalgastosFijos;
+}
+
+function gastosFijosTrabajo() {
+    var valorHerramienta = parseInt(herramienta.value);
+    var valorDominio = parseInt(dominio.value);
+    var valorHosting = parseInt(hosting.value);
+    var valorOtrosTrabajo = parseInt(otrosTrabajo.value);
+
+    // console.log(valorHerramienta);
+    // console.log(valorDominio);
+    // console.log(valorHosting);
+    // console.log(valorOtrosTrabajo);
+    var totalgastosFijosTrabajo = valorHerramienta + valorDominio + valorHosting + valorOtrosTrabajo;
+    return totalgastosFijosTrabajo;
+
+}
